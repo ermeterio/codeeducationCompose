1289c929feae31a953f91fc9718e7c552f92b4f7
daniel.ermeterio <ermeterio@gmail.com>
2022-05-25 22:13:33 -0300

    test

--- app/index.js
+++ app/index.js
@@ -7,6 +7,8 @@ const config = {
     password: 'root',
     database:'nodedb'
 };
+
+
 const mysql = require('mysql2')
 app.get('/', (req,res) => {
   var connection = mysql.createConnection(config)
